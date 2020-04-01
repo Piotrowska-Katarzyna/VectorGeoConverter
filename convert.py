63e1e9e20643202345d6e89e84cca959829f2716
Agnieszka Witkowska <agaw_1614@wp.pl>
2020-04-01 23:01:53 +0200

    Dodano format GeoPackage

--- convert.py
+++ convert.py
@@ -2,11 +2,13 @@ import os
 
 from read_file.from_shp import from_shp
 from read_file.from_geojson import from_geojson
+from read_file.from_geopackage import from_geopackage
 from write_file.to_geojson import to_geojson
 from write_file.to_shapefile import to_shapefile
+from write_file.to_geopackage import to_geopackage
 
-valid_formats = ["shapefile", "geojson"]
-valid_extensions = [".shp", ".geojson"]
+valid_formats = ["shapefile", "geojson", "geopackage"]
+valid_extensions = [".shp", ".geojson", ".gpkg"]
 
 def convert(file, target_format):
     filename, file_extension = os.path.splitext(file)
@@ -15,11 +17,15 @@ def convert(file, target_format):
             gdf = from_shp(file)
         if file_extension == ".geojson":
             gdf = from_geojson(file)
+        if file_extension == ".gpkg":
+            gdf = from_geopackage(file)
 
         if target_format == "geojson":
             to_geojson(gdf, filename)
         if target_format == "shapefile":
             to_shapefile(gdf,filename)
+        if target_format == "geopackage":
+            to_geopackage(gdf, filename)
             
     else:
         print("niepoprawne dane wejściowe")
