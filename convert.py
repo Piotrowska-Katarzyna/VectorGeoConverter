3b316dc7ee448ccc395f507760f6676e83b56cd4
IzabelaSmiech <izabelawsmiech@gmail.com>
2020-04-02 18:37:09 +0200

    dane + poprawki convert

--- convert.py
+++ convert.py
@@ -12,7 +12,7 @@ from write_file.to_csv import to_csv
 valid_formats = ["shapefile", "geojson", "csv"]
 valid_extensions = [".shp", ".geojson", ".csv"]
 
-def convert(file, target_format):
+def convert(file, target_format, x = None, y = None):
     filename, file_extension = os.path.splitext(file)
     if file_extension in valid_extensions and target_format in valid_formats:
         if file_extension == ".shp":
@@ -20,8 +20,11 @@ def convert(file, target_format):
         if file_extension == ".geojson":
             gdf = from_geojson(file)
         if file_extension == ".csv":
-            gdf = from_csv(file, x = True, y = True)
+            if x != None or y != None:
+                gdf = from_csv(file, x, y)
+
         if target_format == "csv":
             to_csv(gdf, filename)
     else:
         print("niepoprawne dane wejściowe")
+
