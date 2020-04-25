cee2184b325412feb096ca2964453694e3a1582a
Agnieszka Witkowska <agaw_1614@wp.pl>
2020-04-25 11:42:13 +0200

    Zwracanie topologi Topojson'a

--- read_file/from_topojson.py
+++ read_file/from_topojson.py
@@ -7,4 +7,6 @@ import json
 def from_topojson(file):
     tj = np.read_topojson(file)
     gdf = topojson.Topology(json, tj, prequantize=False, topology=True)
+    #Zwracanie topologii TopoJSONa dla określonego obiektu geometrycznego
+    #Topology(self, data, topology=True, prequantize=True, topoquantize=False, presimplify=False, toposimplify=0.0001, simplify_with='shapely', simplify_algorithm='dp', winding_order='CW_CCW')
     return gdf
